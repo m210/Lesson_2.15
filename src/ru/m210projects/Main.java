6907664f2cb078f66b73e11cc13be17e04f84342
[M210®] <m210-2007@mail.ru>
2022-03-02 16:12:39 +0300

    Homework  2.15

--- src/ru/m210projects/Main.java
+++ src/ru/m210projects/Main.java
@@ -3,6 +3,17 @@ package ru.m210projects;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        IntArrayList list = new IntArrayList(generateRandomArray(100_000));
+
+        System.out.println(list.contains(list.get(12)));
+    }
+
+    public static Integer[] generateRandomArray(int size) {
+        java.util.Random random = new java.util.Random();
+        Integer[] arr = new Integer[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 }
